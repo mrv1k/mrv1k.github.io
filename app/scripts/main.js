4adcd73dab8925e27f36565410c5ac9db9cb3d57
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2017-06-20 21:22:24 -0400

    Navigation color change on scroll. CSS for that and clean up css comments.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
 $('.navbar-toggler').collapse();
+
+const myNav = document.querySelector('.navbar');
+window.onscroll = function() {
+  if (document.body.scrollTop >= 68) {
+    myNav.classList.add('nav-opaque');
+  } else {
+    myNav.classList.remove('nav-opaque');
+  }
+};
