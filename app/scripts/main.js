992de706eba96d7045eaaeb7d091f02e29d66e56
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2017-06-21 19:11:25 -0400

    Enable form validaiton. Add color feedback using bootstrap defining states.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,25 @@
 $('.navbar-toggler').collapse();
+// override jquery validate plugin defaults
+
+$('.contact-form').validate({
+  errorClass: "help-block",
+  errorElement: "span",
+  highlight: function(element) {
+    $(element).closest('.form-group').addClass('has-danger');
+    $(element).closest('input').addClass('form-control-danger');
+    $('.btn.btn-outline-success').removeClass('btn-outline-success').addClass('btn-outline-danger');
+  },
+  unhighlight: function(element) {
+    $(element).closest('.form-group').removeClass('has-danger');
+    $(element).closest('input').removeClass('form-control-danger');
+  },
+  success: function(element) {
+    $(element).closest('.form-group').addClass('has-success');
+    $(element).closest('input').addClass('form-control-success');
+    $('.btn.btn-outline-danger').removeClass('btn-outline-danger').addClass('btn-outline-success');
+  }
+});
+
 
 const myNav = document.querySelector('.navbar');
 window.onscroll = function() {
