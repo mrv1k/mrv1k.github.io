8bc605b7c50f2a3d4b7923ea0f4df753712271c3
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2017-06-22 15:53:15 -0400

    Submitting form with AJAX

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -17,6 +17,25 @@ $('.contact-form').validate({
     $(element).closest('.form-group').addClass('has-success');
     $(element).closest('input').addClass('form-control-success');
     $('.btn.btn-outline-danger').removeClass('btn-outline-danger').addClass('btn-outline-success');
+  },
+  submitHandler: function(form) {
+    $.ajax({
+      url: 'http://formspree.io/vkhotimchenko@gmail.com',
+      method: 'POST',
+      data: {
+        name: $(form).find("input[name='name']").val(),
+        _replyto: $(form).find("input[name='_replyto']").val(),
+        message: $(form).find("textarea[name='message']").val()
+      },
+      dataType: 'json',
+      success: function() {
+        $("#submit-success").fadeIn();
+        $("#contact-form").fadeOut();
+      },
+      error: function() {
+        $("#submit-errors").fadeIn();
+      }
+    });
   }
 });
 
