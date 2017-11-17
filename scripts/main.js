b1d8a3ec6f2b112e16e68e7c887e0b8887187cb4
Viktor Crow <vkhotimchenko@gmail.com>
2017-11-16 22:18:05 -0500

    Add landing section html/css content, use main.js
    package.json
    Add typed.js dependency
    
    main.js
    Use typed.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,51 +1,58 @@
-$('.navbar-toggler').collapse();
-// override jquery validate plugin defaults
-
-$('.contact-form').validate({
-  errorClass: 'help-block',
-  errorElement: 'span',
-  highlight: function(element) {
-    $(element).closest('.form-group').addClass('has-danger');
-    $(element).closest('input').addClass('form-control-danger');
-    $('.btn.btn-outline-success').removeClass('btn-outline-success').addClass('btn-outline-danger');
-  },
-  unhighlight: function(element) {
-    $(element).closest('.form-group').removeClass('has-danger');
-    $(element).closest('input').removeClass('form-control-danger');
-  },
-  success: function(element) {
-    $(element).closest('.form-group').addClass('has-success');
-    $(element).closest('input').addClass('form-control-success');
-    $('.btn.btn-outline-danger').removeClass('btn-outline-danger').addClass('btn-outline-success');
-  },
-  submitHandler: function(form) {
-    $.ajax({
-      url: '//formspree.io/vkhotimchenko@gmail.com',
-      method: 'POST',
-      data: {
-        name: $(form).find('input[name=\'sender-name\']').val(),
-        _replyto: $(form).find('input[name=\'_replyto\']').val(),
-        message: $(form).find('textarea[name=\'message\']').val()
-      },
-      dataType: 'json',
-      success: function() {
-        $('.contact-form').fadeOut();
-        $('.footer .main-header').text('Message received!');
-        $('.footer .sub-header').text('I\'ll respond ASAP.');
-      },
-      error: function() {
-        $('.submit-errors').fadeIn();
-      }
-    });
+$(document).ready(() => {
+  // Type & erase text effect
+  const typedOptions = {
+    strings: ['I\'m a ^1 programmar', 'I\'m a ^1 programmor', 'I\'m a ^1 programer', '^1000I write code.'],
+    backDelay: 1500,
+    typeSpeed: 60,
+    backSpeed: 50,
+    cursorChar: '',
+    onComplete: () => {
+      console.log('what')
+      $('.v-landing-btn').fadeTo('slow', 1);
+    }
   }
+
+  const typed = new Typed(".v-landing-text-typed", typedOptions);
+
+
+  // override jquery validate plugin defaults
+  $('.contact-form').validate({
+    errorClass: 'help-block',
+    errorElement: 'span',
+    highlight: function (element) {
+      $(element).closest('.form-group').addClass('has-danger');
+      $(element).closest('input').addClass('form-control-danger');
+      $('.btn.btn-outline-success').removeClass('btn-outline-success').addClass('btn-outline-danger');
+    },
+    unhighlight: function (element) {
+      $(element).closest('.form-group').removeClass('has-danger');
+      $(element).closest('input').removeClass('form-control-danger');
+    },
+    success: function (element) {
+      $(element).closest('.form-group').addClass('has-success');
+      $(element).closest('input').addClass('form-control-success');
+      $('.btn.btn-outline-danger').removeClass('btn-outline-danger').addClass('btn-outline-success');
+    },
+    submitHandler: function (form) {
+      $.ajax({
+        url: '//formspree.io/vkhotimchenko@gmail.com',
+        method: 'POST',
+        data: {
+          name: $(form).find('input[name=\'sender-name\']').val(),
+          _replyto: $(form).find('input[name=\'_replyto\']').val(),
+          message: $(form).find('textarea[name=\'message\']').val()
+        },
+        dataType: 'json',
+        success: function () {
+          $('.contact-form').fadeOut();
+          $('.footer .main-header').text('Message received!');
+          $('.footer .sub-header').text('I\'ll respond ASAP.');
+        },
+        error: function () {
+          $('.submit-errors').fadeIn();
+        }
+      });
+    }
+  });
+
 });
-
-
-const myNav = document.querySelector('.navbar');
-window.onscroll = function() {
-  if (document.body.scrollTop >= 68) {
-    myNav.classList.add('nav-opaque');
-  } else {
-    myNav.classList.remove('nav-opaque');
-  }
-};
