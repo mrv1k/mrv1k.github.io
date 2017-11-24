d9611088d004d89f874a6e87ba7b46ce5984e789
Viktor Crow <vkhotimchenko@gmail.com>
2017-11-23 23:24:15 -0500

    Add smooth scroll
    Replace html arrow with svg

--- scripts/main.js
+++ scripts/main.js
@@ -7,12 +7,42 @@ $(document).ready(() => {
     backSpeed: 50,
     cursorChar: '',
     onComplete: () => {
-      console.log('what')
       $('.v-landing-btn').fadeTo('slow', 1);
     }
   }
 
   const typed = new Typed(".v-landing-text-typed", typedOptions);
+
+  // Smooth scroll
+
+  // jQuery Easing
+  //  * Copyright 2008 George McGinley Smith
+  //  * All rights reserved.
+  $.easing.easeInOutExpo = function (x, t, b, c, d) {
+    if (t == 0) return b;
+    if (t == d) return b + c;
+    if ((t /= d / 2) < 1) return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
+    return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
+  }
+
+  const $scroll = $('.v-landing-btn, .v-landing-more');
+  const $root = $('html, body');
+
+  $scroll.click(function (event) {
+    const href = $(this.hash);
+    event.preventDefault();
+
+    $root.animate({ scrollTop: href.offset().top },
+      {
+        duration: 1000,
+        easing: 'easeInOutExpo',
+        step: (now, fx) => {
+          console.log(fx);
+          // console.log(now);
+        }
+      }
+    );
+  });
 
 
   // override jquery validate plugin defaults
