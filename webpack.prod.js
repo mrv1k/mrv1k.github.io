32dccec4dd2c1fe832750e48054e95cfdfbebeeb
Viktor Crow <vkhotimchenko@gmail.com>
2017-11-30 16:58:20 -0500

    HOTFIX! Fix webpack.prod config

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common, {
   devtool: 'source-map',
@@ -10,7 +11,6 @@ module.exports = merge(common, {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
     }),
-
     new UglifyJSPlugin({
       sourceMap: true
     })
