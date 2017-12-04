4b1fb3a298f256e53a6e2b500c28652489f250f1
Viktor Crow <vkhotimchenko@gmail.com>
2017-12-04 00:13:08 -0500

    Separate npm deploy script to a file
    Add 'git-last-commit' dependency to check SHA before deploying

---
+++ ghpages.js
@@ -0,0 +1,20 @@
+const git = require('git-last-commit');
+const ghpages = require('gh-pages');
+
+const myShortHash = '8b5068b';
+
+git.getLastCommit(function(err, commit) {
+  console.log(`${commit.shortHash} vs ${myShortHash}`);
+
+  if (commit.shortHash === myShortHash) {
+    ghpages.publish('dist', {
+      branch: 'master', // self named github repos must be hosted from master
+      message: `dev @ ${myShortHash}`,
+      dotfiles: true // include .gitignore to avoid github node_modules issues
+    });
+    console.log('Published');
+  } else {
+    console.log('NOT DEPLOYED!');
+    console.log('You forgot to update deploy commit hash');
+  }
+});
