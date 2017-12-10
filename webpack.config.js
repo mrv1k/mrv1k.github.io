ffb9149eea7641f3ec28a659404069329f771c6c
Viktor Crow <vkhotimchenko@gmail.com>
2017-12-09 22:12:56 -0500

    chore: Make devConfig composition config

--- webpack.config.js
+++ webpack.config.js
@@ -14,113 +14,108 @@ const PATHS = {
   build: path.join(__dirname, 'build'),
 };
 
-const commonConfig = {
-  entry: PATHS.app,
-  output: {
-    filename: 'bundle.js',
-    path: PATHS.build,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(html)$/,
-        use: {
-          loader: 'html-loader',
-        },
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'images/',
-            },
+const parts = require('./webpack.config.parts');
+
+
+const commonConfig = merge([
+  {
+    entry: PATHS.app,
+    output: {
+      filename: 'bundle.js',
+      path: PATHS.build,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(html)$/,
+          use: {
+            loader: 'html-loader',
           },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'app/index.html',
-    }),
-  ],
-};
-
-
-const prodConfig = {
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg)$/,
           use: [
-            'css-loader',
             {
-              loader: 'postcss-loader',
+              loader: 'file-loader',
               options: {
-                plugins() {
-                  return [uncssPlugin.postcssPlugin({
-                    html: [path.resolve(__dirname, 'app/index.html')],
-                    ignore: [
-                      '.form-control.is-invalid',
-                      '.form-control.is-invalid ~ .invalid-feedback',
-                      '.form-control.is-invalid:focus',
-                    ],
-                  }),
-                  AutoprefixerPlugin()];
-                },
-                sourceMap: true,
+                name: '[name].[ext]',
+                outputPath: 'images/',
               },
             },
           ],
-          fallback: 'style-loader',
-        }),
-      },
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'app/index.html',
+      }),
     ],
   },
-  devtool: 'source-map',
-  plugins: [
-    new CleanWebpackPlugin(['build']),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-    }),
-    new UglifyJSPlugin({
-      sourceMap: true,
-    }),
-    new ExtractTextPlugin({
-      allChunks: true,
-      filename: '[name].css',
-      fallback: 'style-loader',
-    }),
-  ],
-  performance: {
-    hints: 'error',
-  },
-};
+]);
 
 
-const devConfig = {
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
-      },
+const prodConfig = merge([
+  {
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ExtractTextPlugin.extract({
+            use: [
+              'css-loader',
+              {
+                loader: 'postcss-loader',
+                options: {
+                  plugins() {
+                    return [uncssPlugin.postcssPlugin({
+                      html: [path.resolve(__dirname, 'app/index.html')],
+                      ignore: [
+                        '.form-control.is-invalid',
+                        '.form-control.is-invalid ~ .invalid-feedback',
+                        '.form-control.is-invalid:focus',
+                      ],
+                    }),
+                    AutoprefixerPlugin()];
+                  },
+                  sourceMap: true,
+                },
+              },
+            ],
+            fallback: 'style-loader',
+          }),
+        },
+      ],
+    },
+    devtool: 'source-map',
+    plugins: [
+      new CleanWebpackPlugin(['build']),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('production'),
+      }),
+      new UglifyJSPlugin({
+        sourceMap: true,
+      }),
+      new ExtractTextPlugin({
+        allChunks: true,
+        filename: '[name].css',
+        fallback: 'style-loader',
+      }),
     ],
+    performance: {
+      hints: 'error',
+    },
   },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './build',
-    stats: 'errors-only',
-    overlay: true,
+]);
+
+
+const devConfig = merge([
+  parts.devServer(),
+  parts.loadCSS(),
+  {
+    devtool: 'inline-source-map',
   },
-};
+]);
 
 
 module.exports = (env) => {
---
+++ webpack.config.parts.js
@@ -0,0 +1,21 @@
+// DEV
+exports.devServer = () => ({
+  devServer: {
+    stats: 'errors-only',
+    overlay: true,
+  },
+});
+
+exports.loadCSS = ({ include, exclude } = {}) => ({
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        include,
+        exclude,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+});
+
