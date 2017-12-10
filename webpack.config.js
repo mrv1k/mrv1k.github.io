0a7863c58da1053d7099e957a29986d4b9bf3e29
Viktor Crow <vkhotimchenko@gmail.com>
2017-12-09 23:57:41 -0500

    chore: Make prodConfig composition config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,7 @@
-const webpack = require('webpack');
 const merge = require('webpack-merge');
 const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const AutoprefixerPlugin = require('autoprefixer');
-const uncssPlugin = require('uncss');
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
@@ -57,64 +51,36 @@ const commonConfig = merge([
 
 const prodConfig = merge([
   {
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: ExtractTextPlugin.extract({
-            use: [
-              'css-loader',
-              {
-                loader: 'postcss-loader',
-                options: {
-                  plugins() {
-                    return [uncssPlugin.postcssPlugin({
-                      html: [path.resolve(__dirname, 'app/index.html')],
-                      ignore: [
-                        '.form-control.is-invalid',
-                        '.form-control.is-invalid ~ .invalid-feedback',
-                        '.form-control.is-invalid:focus',
-                      ],
-                    }),
-                    AutoprefixerPlugin()];
-                  },
-                  sourceMap: true,
-                },
-              },
-            ],
-            fallback: 'style-loader',
-          }),
-        },
-      ],
-    },
     devtool: 'source-map',
-    plugins: [
-      new CleanWebpackPlugin(['build']),
-      new webpack.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify('production'),
-      }),
-      new UglifyJSPlugin({
-        sourceMap: true,
-      }),
-      new ExtractTextPlugin({
-        allChunks: true,
-        filename: '[name].css',
-        fallback: 'style-loader',
-      }),
-    ],
     performance: {
       hints: 'error',
     },
   },
+  parts.extractCSS({
+    use: [
+      'css-loader',
+      parts.autoprefix(),
+      parts.uncss({
+        html: [path.join(PATHS.app, 'index.html')],
+        ignore: [
+          '.form-control.is-invalid',
+          '.form-control.is-invalid ~ .invalid-feedback',
+          '.form-control.is-invalid:focus',
+        ],
+      }),
+    ],
+  }),
+  parts.clean(PATHS.build),
+  parts.uglifyJS({ sourceMap: true }),
 ]);
 
 
 const devConfig = merge([
-  parts.devServer(),
-  parts.loadCSS(),
   {
     devtool: 'inline-source-map',
   },
+  parts.devServer(),
+  parts.loadCSS(),
 ]);
 
 
--- webpack.config.parts.js
+++ webpack.config.parts.js
@@ -1,3 +1,64 @@
+// DEV variables
+
+// PROD variables
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const AutoprefixerPlugin = require('autoprefixer');
+const UncssPlugin = require('uncss');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+
+exports.clean = path => ({
+  plugins: [new CleanWebpackPlugin(path)],
+});
+
+exports.uglifyJS = ({ sourceMap } = {}) => ({
+  plugins: [new UglifyJSPlugin({ sourceMap })],
+});
+
+exports.extractCSS = ({ include, exclude, use }) => {
+  const extractPlugin = new ExtractTextPlugin({
+    allChunks: true,
+    filename: '[name].css',
+  });
+
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          include,
+          exclude,
+          use: extractPlugin.extract({
+            use,
+            fallback: 'style-loader',
+          }),
+        },
+      ],
+    },
+    plugins: [extractPlugin],
+  };
+};
+
+exports.autoprefix = () => ({
+  loader: 'postcss-loader',
+  options: {
+    plugins() {
+      return [AutoprefixerPlugin()];
+    },
+  },
+});
+
+exports.uncss = ({ html, ignore }) => ({
+  loader: 'postcss-loader',
+  options: {
+    plugins() {
+      return [UncssPlugin.postcssPlugin({ html, ignore })];
+    },
+  },
+});
+
+
 // DEV
 exports.devServer = () => ({
   devServer: {
@@ -18,4 +79,6 @@ exports.loadCSS = ({ include, exclude } = {}) => ({
     ],
   },
 });
+
+// PROD
 
