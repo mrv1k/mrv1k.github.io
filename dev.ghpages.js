90a80f6001bf3f957f123d7d35d365c4bdaeff1e
Viktor Crow <vkhotimchenko@gmail.com>
2017-12-14 01:58:49 -0500

    chore: Replace and remove `git-last-commit`

--- dev.ghpages.js
+++ dev.ghpages.js
@@ -1,20 +1,22 @@
 /* eslint-disable no-console, import/no-extraneous-dependencies */
 
-const git = require('git-last-commit');
-const ghpages = require('gh-pages');
+const GitRevisionPlugin = require('git-revision-webpack-plugin');
 const fs = require('fs');
+const ghpages = require('gh-pages');
 
+const shortHash = new GitRevisionPlugin().version();
 const myShortHash = '5479d2a';
 
-git.getLastCommit((err, commit) => {
-  console.log(' LATEST -- CURRENT');
-  console.log(`${commit.shortHash} vs ${myShortHash}`);
 
-  if (commit.shortHash === myShortHash) {
+function deploy() {
+  console.log(' LATEST -- CURRENT');
+  console.log(`${shortHash} vs ${myShortHash}`);
+
+  if (shortHash === myShortHash) {
     // Push .gitignore to prevent git parsing /node_modules on branch change
-    fs.copyFile('./.gitignore', './build/.gitignore', (fsErr) => {
-      if (fsErr) {
-        throw fsErr;
+    fs.copyFile('./.gitignore', './build/.gitignore', (err) => {
+      if (err) {
+        throw err;
       } else {
         ghpages.publish('build', {
           branch: 'master', // self named github repos must be hosted from master
@@ -28,4 +30,6 @@ git.getLastCommit((err, commit) => {
     console.log('NOT PUBLISHED!');
     console.log('You forgot to update deploy commit hash.');
   }
-});
+}
+
+deploy();
