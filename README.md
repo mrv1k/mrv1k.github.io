37c4f6ec5f97e1685864ebd92e1013800a111111
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-07-20 17:57:48 -0400

    gatsby new blog template

---
+++ README.md
@@ -0,0 +1,12 @@
+# [mrv1k.me](https://mrv1k.me)
+
+My portfolio and blog website
+
+Forked from [gatsby-starter-blog@6aae8c6](https://github.com/gatsbyjs/gatsby-starter-blog/tree/6aae8c6bf337b7ce96d4bd2ef4e3aace181e48a6)
+
+To run locally
+
+```shell
+npm install
+npm run dev
+```
---
+++ src/components/bio.js
@@ -0,0 +1,48 @@
+/**
+ * Bio component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.com/docs/use-static-query/
+ */
+
+import * as React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
+
+const Bio = () => {
+  const data = useStaticQuery(graphql`
+    query BioQuery {
+      site {
+        siteMetadata {
+          author {
+            name
+            summary
+          }
+        }
+      }
+    }
+  `);
+
+  // Set these values by editing "siteMetadata" in gatsby-config.js
+  const author = data.site.siteMetadata?.author;
+
+  return (
+    <div className="bio">
+      <StaticImage
+        className="bio-avatar"
+        layout="fixed"
+        formats={["AUTO", "WEBP", "AVIF"]}
+        src="../images/mrv1k-pic.jpg"
+        width={52}
+        height={52}
+        quality={95}
+        alt={author?.name}
+      />
+      <p style={{ maxWidth: "310px" }}>
+        Blog by {author?.name} <br />A journey to teach myself CS.
+      </p>
+    </div>
+  );
+};
+
+export default Bio;
