1d96c1d4bf36152fdbe715af6b14fe6562f4ed7c
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-07-20 20:58:20 -0400

    feat: add f counter to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,14 +4,23 @@ import { graphql } from "gatsby";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 
+// TODO: easter egg: do a piano with only f working
 const NotFoundPage = ({ data, location }) => {
+  const [fCount, setCount] = React.useState(0);
   const siteTitle = data.site.siteMetadata.title;
+  const onClick = (e) => {
+    if (fCount > 404) return;
+    setCount(fCount + 1);
+  };
 
   return (
     <Layout location={location} title={siteTitle}>
       <Seo title="404: Not Found" />
       <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <button onClick={onClick} value={fCount}>
+        f
+      </button>
+      <p>{fCount}</p>
     </Layout>
   );
 };
