ecb82b7f77e6ff1ee4d4303f285e0bad30b009ad
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-07-21 22:05:40 -0400

    wip

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,28 +1,32 @@
 import * as React from "react";
 import Seo from "../components/seo";
 import * as styles from "./portfolio.module.css";
+import mrv1k_pic from "../images/mrv1k-pic-large.jpg";
+
+const cheese_lorem = `Fromage frais croque monsieur stinking bishop. Cheeseburger chalk and cheese bocconcini ricotta bocconcini squirty cheese monterey jack cheesy grin. Paneer cheese strings rubber cheese taleggio gouda macaroni cheese ricotta port-salut. Rubber cheese fromage frais swiss paneer stinking bishop the big cheese smelly cheese bocconcini. Airedale.`;
 
 const PortfolioPage = () => {
   const title = "Viktor Khotimchenko Portfolio";
 
   return (
-    <div>
+    <div class={styles.wrapper}>
       <Seo title={title} />
       <main className={styles.sections}>
+        <img className={styles.imageOfMe} src={mrv1k_pic} />
         <section className="about">
-          <div className={styles.content}>about</div>
+          <div className={styles.content}>1 {cheese_lorem}</div>
         </section>
         <section className="skills">
-          <div className={styles.content}>skills</div>
+          <div className={styles.content}>2 {cheese_lorem}</div>
         </section>
         <section className="projects">
-          <div className={styles.content}>projects</div>
+          <div className={styles.content}>3 {cheese_lorem}</div>
         </section>
         <section className="experience">
-          <div className={styles.content}>experience</div>
+          <div className={styles.content}>4 {cheese_lorem}</div>
         </section>
         <section className="contact">
-          <div className={styles.content}>contact</div>
+          <div className={styles.content}>5 {cheese_lorem}</div>
         </section>
       </main>
     </div>
