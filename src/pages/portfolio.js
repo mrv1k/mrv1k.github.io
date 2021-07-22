2d30c5da49b65da431f0e8e7f4567d342cd69d6b
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-07-22 18:12:38 -0400

    refactor: use plain css not, module css

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import Seo from "../components/seo";
-import * as styles from "./portfolio.module.css";
 import mrv1k_pic from "../images/mrv1k-pic-large.jpg";
+import "./portfolio.css";
 
 const cheese_lorem = `Fromage frais croque monsieur stinking bishop. Cheeseburger chalk and cheese bocconcini ricotta bocconcini squirty cheese monterey jack cheesy grin. Paneer cheese strings rubber cheese taleggio gouda macaroni cheese ricotta port-salut. Rubber cheese fromage frais swiss paneer stinking bishop the big cheese smelly cheese bocconcini. Airedale.`;
 
@@ -11,25 +11,25 @@ const PortfolioPage = () => {
   return (
     <main>
       <Seo title={title} />
-      <section className={styles.about}>
-        <div className={styles.content}>1 {cheese_lorem}</div>
+      <section className="about">
+        <div>1 {cheese_lorem}</div>
       </section>
       <img
-        className={styles.imageOfMe}
+        className="image-of-me"
         src={mrv1k_pic}
         alt="unoptimized img of me"
       />
       <section className="skills">
-        <div className={styles.content}>2 {cheese_lorem}</div>
+        <div>2 {cheese_lorem}</div>
       </section>
       <section className="projects">
-        <div className={styles.content}>3 {cheese_lorem}</div>
+        <div>3 {cheese_lorem}</div>
       </section>
       <section className="experience">
-        <div className={styles.content}>4 {cheese_lorem}</div>
+        <div>4 {cheese_lorem}</div>
       </section>
       <section className="contact">
-        <div className={styles.content}>5 {cheese_lorem}</div>
+        <div>5 {cheese_lorem}</div>
       </section>
     </main>
   );
