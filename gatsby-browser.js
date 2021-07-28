3c9ecefb010d26ad1264e9fc92d7d889be96256c
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-07-28 11:24:36 -0400

    style: add color scheme, add atom prism theme

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -9,8 +9,7 @@ import "./src/styles/normalize.css";
 import "./src/styles/style.css";
 
 // Highlighting for code blocks
-import "prismjs/themes/prism-solarizedlight.css";
-
+import "./src/styles/atom-dark.css";
 import "prismjs/plugins/line-numbers/prism-line-numbers.css";
 import "prismjs/plugins/command-line/prism-command-line.css";
 import "./src/styles/syntax-highlight.css";
